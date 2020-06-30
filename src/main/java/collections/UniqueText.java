017fedcb8e52a61dad2363f59f808351d0689b2d
astolbin <astolbin@ya.ru>
2020-06-30 08:00:14 +0300

    2. Идентичные тексты. [#213688]

---
+++ src/main/java/collections/UniqueText.java
@@ -0,0 +1,25 @@
+package collections;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+
+        for (String word : origin) {
+            check.add(word);
+        }
+
+        for (String duplicate : text) {
+            if (check.add(duplicate)) {
+                rsl = false;
+                break;
+            }
+        }
+
+        return rsl;
+    }
+}
